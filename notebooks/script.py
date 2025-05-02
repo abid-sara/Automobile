ccf311cc1b593284b6c443f6925a0e7c05e189ae
abid-sara <sara.abid@ensia.edu.dz>
2025-05-02 22:15:34 +0100

    fixed path in script

--- notebooks/script.py
+++ notebooks/script.py
@@ -11,7 +11,7 @@ notebooks = [
 merged_nb = nbmerge.merge_notebooks(file_paths=notebooks, base_dir=".")
 
 # Write the merged notebook to a UTF-8 file
-with open("automobile.ipynb", "w", encoding="utf-8") as f:
+with open("notebooks\\automobile.ipynb", "w", encoding="utf-8") as f:
     nbmerge.write_notebook(merged_nb, f)
 
 print("Merging completed: automobile.ipynb")
