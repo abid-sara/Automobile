ec8f2bcfc579d605ea02ae9140d0ff2c41996675
LyesHADJAR <lyes.hadjar@ensia.edu.dz>
2025-05-10 18:53:37 +0100

    merged the ann notebook with the initial notebook

--- notebooks/script.py
+++ notebooks/script.py
@@ -2,15 +2,15 @@ import nbmerge
 
 # List all your notebook filenames
 notebooks = [
-    "notebooks\\automobile.ipynb",
-    "notebooks\\naive_bayes.ipynb",
+    "notebooks/automobile.ipynb",
+    "notebooks/neural_network.ipynb",
 ]
 
 # Merge the notebooks
 merged_nb = nbmerge.merge_notebooks(file_paths=notebooks, base_dir=".")
 
 # Write the merged notebook to a UTF-8 file
-with open("notebooks\\automobile.ipynb", "w", encoding="utf-8") as f:
+with open("notebooks/automobile.ipynb", "w", encoding="utf-8") as f:
     nbmerge.write_notebook(merged_nb, f)
 
 print("Merging completed: automobile.ipynb")
